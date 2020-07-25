a48165f39bb94e3d5a7d989b25ab8b4cb302db3a
Rafael Rossi Debarba <rafadeross@hotmail.com>
2020-07-25 18:52:31 -0300

    Tempo limite de funcionamento

--- motobomba.ino
+++ motobomba.ino
@@ -1,14 +1,24 @@
 // Neste projeto o motor tem a fase e o neutro ligado e desligado juntos
-int porta_motor_primeira = 10;
-int porta_motor_segunda = 11;
+#define porta_motor 9
 
 // Portas dos sensores
-int sen_poco_baixo = 5;
-int sen_poco_alto = 6;
-int sen_caixa_baixo = 7;
-int sen_caixa_alto = 8;
+#define  sen_poco_baixo 2
+#define sen_poco_alto 3
+#define sen_caixa_baixo 4
+#define sen_caixa_alto 5
 
-int led = 9; // Saída do led que é ativado quando o poço está desativado
+// Portas indicaçao dos leds
+#define led_poco_baixo 6
+#define led_poco_alto 7
+#define led_caixa_baixo 8
+#define led_caixa_alto 10
+
+// Led de problema
+#define led 11
+
+#define max_unsigned_long 4294967295
+
+bool led_pisca = false;
 
 //Poço começa ativado
 bool poco = true;
@@ -25,14 +35,29 @@ bool status_caixa_alto = false;
 bool boia_baixa = true;
 bool boia_alta = false;
 
+bool debug = true;
+
+bool protecao = false;
+
+// time
+unsigned long tempo_ligado_motor = 0;
+const unsigned long tempo_maximo_motor_ligado = 300000; // Em milissegundos
+
 void setup() {
-  pinMode(porta_motor_primeira, OUTPUT);
-  pinMode(porta_motor_segunda, OUTPUT);
+  pinMode(porta_motor, OUTPUT);
   pinMode(sen_poco_baixo, INPUT);
   pinMode(sen_poco_alto, INPUT);
   pinMode(sen_caixa_baixo, INPUT);
   pinMode(sen_caixa_alto, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(led_poco_baixo, OUTPUT);
+  pinMode(led_poco_alto, OUTPUT);
+  pinMode(led_caixa_baixo, OUTPUT);
+  pinMode(led_caixa_alto, OUTPUT);
+
+  digitalWrite(led, HIGH);
+
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -42,26 +67,141 @@ void loop() {
   status_caixa_baixo = digitalRead(sen_caixa_baixo);
   status_caixa_alto = digitalRead(sen_caixa_alto);
 
-  if(!poco && status_poco_alto == boia_alta && status_poco_baixo == boia_alta){ //Ativa o poço
-    poco = true;
+  if(!poco) {
     digitalWrite(led, LOW);
+    delay(100);
+    digitalWrite(led, HIGH);
+  }
+
+  statusLed();
+
+  tempoMaximoMotor();
+
+  if(debug) {
+    printStatus();
+  }
+
+  if(!poco && status_poco_alto == boia_alta && status_poco_baixo == boia_alta && !protecao){ //Ativa o poço
+    poco = true;
   }
   if(poco && status_poco_baixo == boia_baixa) { // desativa o poço
     poco = false;
-    digitalWrite(led, HIGH);
-    digitalWrite(porta_motor_primeira, LOW);
-    digitalWrite(porta_motor_segunda, LOW);
-    motor = false;
+    desligarMotor();
   }
-  if(status_caixa_baixo == boia_baixa && poco) { // liga o motor
-    digitalWrite(porta_motor_primeira, HIGH);
-    digitalWrite(porta_motor_segunda, HIGH);
-    motor = true;
+  if(status_caixa_baixo == boia_baixa && poco && !protecao) { // liga o motor
+    ligarMotor();
   }
   if(status_caixa_alto == boia_alta && motor) { // desliga o motor quando a caixa estiver cheia
-    digitalWrite(porta_motor_primeira, LOW);
-    digitalWrite(porta_motor_segunda, LOW);
-    motor = false;
+    desligarMotor();
   }
   delay(1000);
+}
+
+void ligarMotor() {
+  digitalWrite(porta_motor, HIGH);
+  if(!motor) {
+    tempo_ligado_motor = millis(); 
+  }
+  motor = true;
+  if(debug){
+    Serial.println("Motor Ligado"); 
+  }
+}
+
+void desligarMotor() {
+  digitalWrite(porta_motor, LOW);
+  motor = false;
+  tempo_ligado_motor = 0;
+  testeMotorPosDesl();
+  if(debug){
+    Serial.println("Motor desligado"); 
+  }
+}
+
+void tempoMaximoMotor() {
+  if(motor) {
+    if(millis() > tempo_ligado_motor) {
+      if((millis() - tempo_ligado_motor) >= tempo_maximo_motor_ligado) {
+        desligarMotor();
+      }
+    }
+    else {
+      if(((max_unsigned_long - tempo_ligado_motor) + millis()) >= tempo_maximo_motor_ligado) {
+        desligarMotor();
+      }
+    }
+  }
+}
+
+void testeMotorPosDesl() {
+  if(status_caixa_baixo == boia_baixa) {
+    protecao = true;
+  }
+}
+
+void printStatus() {
+  if(status_poco_baixo) {
+    Serial.print("Poço Baixo TRUE   ");  
+  }
+  else {
+    Serial.print("Poço Baixo FALSE  ");
+  }
+  if(status_poco_alto) {
+    Serial.print("Poço Alto True   "); 
+  }
+  else {
+    Serial.print("Poço Alto False  ");
+  }
+  if(status_caixa_baixo) {
+    Serial.print("Caixa Baixo True   ");
+  }
+  else {
+    Serial.print("Caixa Baixo False  ");
+  }
+  if(status_caixa_alto) {
+    Serial.print("Caixa Alto True   ");
+  }
+  else {
+    Serial.print("Caixa Alto False  ");
+  }
+  if(motor) {
+    Serial.print("Motor Ligado      "); 
+  }
+  else {
+    Serial.print("Motor Desligado   ");
+  }
+  if(protecao) {
+    Serial.println("Protecao ligada       ");
+  }
+  else {
+    Serial.println("Protecao desligada    ");
+  }
+  Serial.println(millis());
+}
+
+void statusLed() {
+  if(status_poco_baixo) {
+    digitalWrite(led_poco_baixo, HIGH);
+  }
+  else {
+    digitalWrite(led_poco_baixo, LOW);
+  }
+  if(status_poco_alto) {
+    digitalWrite(led_poco_alto, HIGH);
+  }
+  else {
+    digitalWrite(led_poco_alto, LOW);
+  }
+  if(status_caixa_baixo) {
+    digitalWrite(led_caixa_baixo, HIGH);
+  }
+  else {
+    digitalWrite(led_caixa_baixo, LOW);
+  }
+  if(status_caixa_alto) {
+    digitalWrite(led_caixa_alto, HIGH);
+  }
+  else {
+    digitalWrite(led_caixa_alto, LOW);
+  }
 }
